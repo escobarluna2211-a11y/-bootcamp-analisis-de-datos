96042f28bd19a2bbde9869ad8afb1b874cae0d7f
escobarluna2211-a11y <escobarluna2211@gmail.com>
2026-05-20 20:58:10 -0500

    Add files via upload

---
+++ app.py
@@ -0,0 +1,7 @@
+import pandas as pd
+
+
+ruta1 ='https://github.com/escobarluna2211-a11y/-bootcamp-analisis-de-datos/raw/refs/heads/main/ANM_RUCOM_Explotador_Minero_Autorizado-Solicitudes_De_Legalizaci%C3%B3n_Formalizaci%C3%B3n_20260513.csv'
+
+df1 = pd.read_csv(ruta1)
+
